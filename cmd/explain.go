683ce95dbe53110505700b2c1fcfa8905782ae8d
Sultan Iman <sultan.imanhodjaev@gmail.com>
2021-11-27 10:57:40 +0100

    Add some colors to explanation

--- cmd/explain.go
+++ cmd/explain.go
@@ -2,19 +2,34 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"github.com/imanhodjaev/pwc/card"
 	"github.com/spf13/cobra"
 )
+
+const algorithmDescription = `
+1. Render the first row with shuffled "■□▲△○●★☂☀☁☹☺♠♣♥♦♫€¥£$!?¡¿⊙◐◩�",
+2. Iterate over the rest of the rows
+    a. If the card should include symbols then
+       use the alphanumeric and symbols alphabet for every even character
+       and use the alphanumeric alphabet for the odd columns,
+    b. If the card should have a digits area then
+       use the numeric alphabet for the lower half of the rows 5-8.
+`
 
 var explainCmd = &cobra.Command{
 	Use:   "explain",
 	Short: "Describe algorithm and show alphabets",
 	Long:  "Describe algorithm and show alphabets",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%30s %s\n", "Header symbols:", card.ClassicHeaderRow)
-		fmt.Printf("%30s %s\n", "Numbers:", card.Numbers)
-		fmt.Printf("%30s %s\n", "Symbols:", card.Symbols)
-		fmt.Printf("%30s %s\n", "Alphanumeric:", card.AlphaNumeric)
-		fmt.Printf("%30s %s\n", "Alphanumeric and symbols:", card.AlphaNumericAndSymbols)
+		headerColor := color.New(color.FgGreen, color.Bold).Add(color.Underline)
+		alphabetColor := color.New(color.FgYellow, color.Bold)
+		_, _ = headerColor.Print("Alphabet\n\n")
+		fmt.Printf("%30s %s\n", "Header symbols:", alphabetColor.Sprintf(card.ClassicHeaderRow))
+		fmt.Printf("%30s %s\n", "Numbers:", alphabetColor.Sprintf(card.Numbers))
+		fmt.Printf("%30s %s\n", "Alphanumeric:", alphabetColor.Sprintf(card.AlphaNumeric))
+		fmt.Printf("%30s %s\n", "Alphanumeric and symbols:", alphabetColor.Sprintf(card.AlphaNumericAndSymbols))
+		_, _ = headerColor.Print("\nAlgorithm")
+		fmt.Printf("\n%s", algorithmDescription)
 	},
 }
--- go.mod
+++ go.mod
@@ -13,10 +13,14 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
